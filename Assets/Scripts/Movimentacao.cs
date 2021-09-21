3d743e186e6d5ab442dc50d6e2146167ccb53046
iagoracode <iagorenator@gmail.com>
2021-09-21 16:55:50 -0300

    Scripts de Movimentação e Spawn

--- Assets/Scripts/Movimentacao.cs
+++ Assets/Scripts/Movimentacao.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ola : MonoBehaviour
+public class Movimentacao : MonoBehaviour
 {
+
+    public float velocidade = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("opa");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("opa");
+        if (Input.GetKey(KeyCode.Space))
+        {
+        transform.position += new Vector3(velocidade * Time.deltaTime, 0.0f, 0.0f);
+        }
     }
 }
